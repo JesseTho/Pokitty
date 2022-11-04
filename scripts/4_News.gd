6b22dc65000faec4211ac4494f027d091a72a7ec
RonaldDeese <rdeese@hawaii.edu>
2022-11-03 21:41:49 -1000

    Updates to button styles in other scenes

--- scripts/4_News.gd
+++ scripts/4_News.gd
@@ -4,7 +4,7 @@ extends Node2D
 func _ready(): #New Day Begins
 	GameManager.newDay()
 
-	$HBoxContainer/Popup/Events/EventInfo.text = str(GameManager.eventName[GameManager.event])
+	$HBoxContainer/Popup/Events2/EventInfo.text = str(GameManager.eventName[GameManager.event])
 
 func _on_Button_pressed():
 	queue_free() #closes the news.
