456400700dc9f91461054d101c4b9dce3d00cb7e
JesseTho <JesseTho@hawaii.edu>
2022-11-07 16:13:23 -1000

    Fixed Money Ending

--- scripts/14_MoneyEnding.gd
+++ scripts/14_MoneyEnding.gd
@@ -8,7 +8,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	$HBoxContainer/Popup/Title.text = "Congratulations! You gained $%0.2f" % GameManager.money + " in "+ str(currentDay) + " days!"
+	$HBoxContainer/Popup/Title.text = "Congratulations! You gained $%0.2f" % GameManager.money + " in "+ str(GameManager.currentDay) + " days!"
 	pass # Replace with function body.
 
 
